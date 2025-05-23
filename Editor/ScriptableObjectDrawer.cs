4de46f6c95533613173ed9b4661e953782bffd5e
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-05-23 10:30:50 +0200

    ⠀⠀⠀⠀⠀

--- Editor/ScriptableObjectDrawer.cs
+++ Editor/ScriptableObjectDrawer.cs
@@ -4,11 +4,28 @@ using UnityEditor;
 
 namespace Unity.Essentials
 {
+    /// <summary>
+    /// Provides a custom property drawer for fields of type <see cref="ScriptableObject"/> in the Unity Editor.
+    /// </summary>
+    /// <remarks>This drawer allows for the inline editing of <see cref="ScriptableObject"/> instances in the
+    /// Inspector. When the property is expanded, the drawer displays the Inspector GUI for the referenced <see
+    /// cref="ScriptableObject"/>.</remarks>
     [CustomPropertyDrawer(typeof(ScriptableObject), true)]
     public class ScriptableObjectDrawer : PropertyDrawer
     {
-        private Editor editor = null;
+        private Editor _editor = null;
 
+        /// <summary>
+        /// Renders the custom GUI for a serialized property in the Unity Editor.
+        /// </summary>
+        /// <remarks>This method customizes the rendering of a serialized property in the Unity Editor. 
+        /// If the property has no assigned object reference, it displays a standard property field and collapses any
+        /// expanded state. If the property has an object reference, it displays a foldout to expand or collapse
+        /// additional details. When expanded, the method renders the inspector GUI for the referenced object using a
+        /// cached editor instance.</remarks>
+        /// <param name="position">The rectangle on the screen to use for the property GUI.</param>
+        /// <param name="property">The serialized property to render.</param>
+        /// <param name="label">The label to display alongside the property field.</param>
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             EditorGUI.PropertyField(position, property, label, true);
@@ -25,10 +42,10 @@ namespace Unity.Essentials
 
             EditorGUI.indentLevel++;
             {
-                if (editor == null || editor.target != property.objectReferenceValue)
-                    Editor.CreateCachedEditor(property.objectReferenceValue, null, ref editor);
+                if (_editor == null || _editor.target != property.objectReferenceValue)
+                    Editor.CreateCachedEditor(property.objectReferenceValue, null, ref _editor);
 
-                editor?.OnInspectorGUI();
+                _editor?.OnInspectorGUI();
             }
             EditorGUI.indentLevel--;
         }
